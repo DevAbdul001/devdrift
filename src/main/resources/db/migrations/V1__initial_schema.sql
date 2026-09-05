62daeab77cae86fec0525d99a2bf91d73cd94d3e
DevAbdul001 <abdurahmanali363@gmail.com>
2026-09-05 15:17:08 +0300

    chore : migration v1 defined

---
+++ src/main/resources/db/migrations/V1__initial_schema.sql
@@ -0,0 +1,24 @@
+
+
+CREATE TABLE IF NOT EXISTS sessions (
+	session_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+	started_at DATETIME NOT NULL,
+	ended_at DATETIME,
+	status ENUM ('STARTED', 'PAUSED', 'ENDED', 'CANCELLED'),
+	notes TEXT,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS session_pauses (
+	pause_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+	session_id BIGINT NOT NULL,
+	started_at DATETIME,
+	ended_at DATETiME,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	
+	FOREIGN KEY (session_id) REFERENCES sessions(session_id)
+);
+
+CREATE INDEX session_pause_index ON session_pauses(session_id);
