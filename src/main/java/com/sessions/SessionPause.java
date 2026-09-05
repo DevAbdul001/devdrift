b7b3d11275a20e427d434c306a5b0d6bba3be715
DevAbdul001 <abdurahmanali363@gmail.com>
2026-09-05 22:33:07 +0300

    chore : add sessionpause entity

---
+++ src/main/java/com/sessions/SessionPause.java
@@ -0,0 +1,59 @@
+
+
+import java.util.*;
+import java.time; 
+
+public class SessionPause{
+	
+	private Long pauseId;
+	private Long sessionId;
+	private LocalDateTime startedAt;
+	private LocalDateTime endedAt;
+
+	public SessionPause(
+		Long pauseId,
+		Long sessionId,
+		LocalDateTime startedAt,
+		LocalDateTime endedAt
+			)
+	{
+		this.pauseId = pauseId;
+		this.sessionId = sessionid;
+		this.startedAt = startedAt;
+		this.endedAt = endedAt;
+	}
+
+	// Setters ===============================================================================
+	public void setPauseId( Long id ){
+		pauseId = id;
+	}
+
+	public void setSessionId ( Long id ){
+		sessionId = id;
+	}
+
+	public void setStartedAt ( LocalDateTime time ){
+		startedAt = time;
+	}
+
+	public void setEndedAt ( LocalDateTime time ) {
+		endedAt = time;
+	}
+
+	// Getters ===============================================================================
+	public Long getPauseid(){
+		return pauseId;
+	}
+
+	public Long getSessionId(){ 
+		return sessionId 
+	}
+
+	public LocalDateTime getStartedAt(){
+		return startedAt;
+	}
+
+	public LocalDateTime getEndedAt(){
+		return endedAt;
+	}
+}
