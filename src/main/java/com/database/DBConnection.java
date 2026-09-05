49c5d3ce6ae74cf0e6236a2e642b388b0f42489d
DevAbdul001 <abdurahmanali363@gmail.com>
2026-09-05 20:22:55 +0300

    chore : add db connection

---
+++ src/main/java/com/database/DBConnection.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+import java.sql.*;
+
+public class DBConnection{
+	
+	String url = System.getenv("DEVDRIFT_URL");
+	String user = System.getenv("DEVDRIFT_USER");
+	String password = System.getenv("DEVDRIFT_PASSWORD");
+
+	public Connection getConnection(){
+		
+	try{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection myConn = DriverManager.getConnection(url,user,password);
+		return myConn;
+
+	} catch (Exception e){
+		throw new RuntimeException("Something went wrong!");
+	}
+
+	}
+
+}
